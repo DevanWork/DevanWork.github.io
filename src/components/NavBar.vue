2f1dfd84ec1822d7838879662b5fad807f42d730
Video <Video@DESKTOP-NLSL16N.local>
2023-01-23 18:08:22 -0500

    added logo

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <b-nav vertical class="beep">
+    <img src="https://media.discordapp.net/attachments/814022268400828446/1067218483928125510/Logo2.png?width=676&height=676" alt="Logo" style="width:250px;height250px;">
       <b-nav-item active>Active</b-nav-item>
       <b-nav-item>Link</b-nav-item>
       <b-nav-item>Another Link</b-nav-item>
